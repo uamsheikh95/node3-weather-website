f41aa9bf0f95a6dab7f0f0f5166b569df09425f4
uamsheikh <uamsheikh95@gmail.com>
2020-01-01 15:29:13 +0300

    Extrac the course zip because i face some problems related to heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/f550fd96678c84c0dbee1c12feff58ef/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
